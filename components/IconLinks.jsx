fc0a2e9d0dd553c2ebdb4975223897c04d61f5e2
davenpos <davenpos@uwindsor.ca>
2025-05-28 16:48:04 -0400

    Added a hover effect when you hover over the project names and made a couple other minor changes to the projects section

--- components/IconLinks.jsx
+++ components/IconLinks.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function IconLinks({ Icon, link }) {
     return (<>
         {link && (<Link href={link} target="_blank" rel="noopener noreferrer">
-            <Icon size={25} className="text-indigo-600 hover:text-indigo-700 transition-300" />
+            <Icon size={25} className="text-indigo-600 hover:text-indigo-700 transition-all duration-300" />
         </Link>)}
     </>)
 }
--- components/Project.jsx
+++ components/Project.jsx
@@ -7,7 +7,7 @@ import getLogo from "@/functions/getLogo"
 export default function Project({ project }) {
     return (<div
         className="bg-white rounded-xl shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
-        <div className="w-full relative aspect-video">
+        <div className="w-full relative aspect-video hover:cursor-pointer">
             <Image
                 src={project.image}
                 alt="Project Alpha"
@@ -15,7 +15,12 @@ export default function Project({ project }) {
             />
         </div>
         <div className="p-6">
-            <h3 className="text-2xl font-semibold text-indigo-600 mb-2">{project.name}</h3>
+            <div className="group relative inline-block mb-2">
+                <h3 className="text-2xl font-semibold text-indigo-600 group-hover:text-indigo-700 cursor-pointer transition-colors duration-300">
+                    {project.name}
+                </h3>
+                <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-indigo-700 transition-all duration-300 group-hover:w-full"></span>
+            </div>
             <p className="text-gray-600 mb-4">{project.description.brief}</p>
             <div className="flex flex-wrap space-x-1">
                 {project.skills.map(skill => {
