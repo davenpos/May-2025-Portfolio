0f4da57c41ec282c1191ce4e99c89e5b3c6ce2fb
davenpos <davenpos@uwindsor.ca>
2025-05-28 17:43:20 -0400

    Added work experience section

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import About from "@/components/About"
 import ProjectsList from "@/components/ProjectsList"
 import SkillsList from "@/components/SkillsList"
+import WorkExperience from "@/components/WorkExperience"
 
 export default function Page() {
     return (<>
         <About />
         <ProjectsList />
         <SkillsList />
+        <WorkExperience />
     </>)
 }
---
+++ components/WorkExperience.jsx
@@ -0,0 +1,51 @@
+import Job from "./Job"
+
+export default function WorkExperience() {
+    const jobs = [
+        {
+            title: "Web Developer",
+            company: "PROJXON",
+            dates: {
+                start: "Mar 2025",
+                end: "Present"
+            },
+            bullets: [
+                "Developed responsive, cross-browser compatible websites using React, Next.js and headless WordPress.",
+                "Enhanced user experience by implementing responsive web design and optimizing website performance.",
+                "Participated in agile sprints and weekly participation meetings."
+            ]
+        },
+        {
+            title: "AI Trainer",
+            company: "DataAnnotation",
+            dates: {
+                start: "May 2024",
+                end: "Present"
+            },
+            bullets: [
+                "Training and optimizing chatbot models using Python, JavaScript, C++, etc.",
+                "Creating structured datasets (CSV, JSON, XML) for AI response evaluation.",
+                "Participated in agile sprints and weekly participation meetings."
+            ]
+        },
+        {
+            title: "Work Placement",
+            company: "Lakehead Public School Board",
+            dates: {
+                start: "Mar 2019",
+                end: "May 2019"
+            },
+            bullets: [
+                "Designed and implemented around 5 custom WordPress themes for school websites.",
+                "Integrated plugins into websites to enhance site usability and functionality.",
+                "Generated JasperSoft reports to streamline reporting."
+            ]
+        }
+    ]
+    return (<section id="experience" className="py-16">
+        <h2 className="section-title text-4xl font-bold text-center mb-12 text-gray-800">Work Experience</h2>
+        <div className="space-y-8 max-w-3xl mx-auto">
+            {jobs.map((job, i) => <Job key={i} job={job} />)}
+        </div>
+    </section>)
+}
