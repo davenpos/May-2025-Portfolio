1acdc15e88783a637ca19b0a51c108674ef546c7
davenpos <davenpos@uwindsor.ca>
2025-05-29 18:11:58 -0400

    Added back SD.jpg that mistakenly got deleted

--- components/ContactLink.jsx
+++ components/ContactLink.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
 
 export default function ContactLink({ link, Icon }) {
-    console.log(Icon)
     return (<Link href={link} target="_blank" rel="noopener noreferrer"
         className="hover:text-indigo-600 transition duration-200">
         <Icon size={30} />
