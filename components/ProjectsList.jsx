418353c7bfe61150ce40987890966b167f05cded
davenpos <davenpos@uwindsor.ca>
2025-06-04 19:31:47 -0400

    Added Zephyr Aero Leather project to portfolio

--- components/ProjectsList.jsx
+++ components/ProjectsList.jsx
@@ -4,6 +4,17 @@ import SectionTitle from "./SectionTitle"
 export default function ProjectsList() {
     const projects = [
         {
+            name: "Zephyr Aero Leather",
+            image: "/images/projects/zephyr-aero-leather.PNG",
+            description: {
+                brief: "An e-commerce site built with Next.js, React and Tailwind, utilizing Stripe for payment and a headless WooCommerce",
+                long: "This was a site I helped build at PROJXON. It is an e-commerce site that sells leather products such as wallets, belts, bags and cases. The site is built with Next.js, React and Tailwind. It utilizes Stripe for payment and is synced to a headless WooCommerce. Users can log in or sign up or simply shop as a guest. Logged in users can also view their order history. I worked on various parts of the site including the order history and payment pages and the Stripe integration."
+            },
+            skills: ["HTML", "CSS", "JavaScript", "PHP", "Next.js", "React", "Tailwind", "WordPress", "WooCommerce", "Stripe", "AWS EC2"],
+            link: "https://zephyraeroleather.com/",
+            code: "https://github.com/PROJXON/zephyr-aero-leather"
+        },
+        {
             name: "PROJXON website",
             image: "/images/projects/projxon-website.png",
             description: {
@@ -22,16 +33,6 @@ export default function ProjectsList() {
                 long: "Inspired by a couple of Node.js applications I had built in the summer prior, I created another Node.js project that allows users to create a piece of sheet music, similar to Finale. Users can log in or sign up, create a very simple piece of sheet music and display it in the web browser. The application runs on an Express server, utilizes an MVC framework and EJS for the views and stores all information into a MySQL database. All musical notation is displayed using special characters in a font called Bravura Text."
             },
             skills: ["HTML", "CSS", "JavaScript", "Node.js", "Express", "MySQL", "SCSS", "EJS", "Webpack"],
-            code: "https://github.com/davenpos/Sheet-Music-Project"
-        },
-        {
-            name: "Ofori Beauty theme",
-            image: "/images/projects/Ofori Beauty theme.png",
-            description: {
-                brief: "A custom classic WordPress theme built with WooCommerce with a blog section, a 404 page, and a JavaScript-powered live search.",
-                long: "A custom WordPress site I built for Ofori Beauty as part of Riipen's Level Up program. I created the theme from scratch, designed it to look like the original site at the time, with some new features such as a blog section, a 404 page, a live search and a custom color picker. It also utilized WooCommerce, SCSS, jQuery, Font Awesome and mobile-first responsive design. It took about a month to build and I was paid $1400 to make it."
-            },
-            skills: ["HTML", "CSS", "PHP", "JavaScript", "WordPress", "WooCommerce", "SCSS", "jQuery"],
             code: "https://github.com/davenpos/Sheet-Music-Project"
         }
     ]
