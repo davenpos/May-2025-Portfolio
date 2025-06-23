874e6d343624ed2fdd46d538f1f2d552e8fa7e4b
davenpos <davenpos@uwindsor.ca>
2025-06-23 17:02:07 -0400

    Updated pictures for PROJXON and ZAL projects

--- components/ProjectsList.jsx
+++ components/ProjectsList.jsx
@@ -5,7 +5,7 @@ export default function ProjectsList() {
     const projects = [
         {
             name: "Zephyr Aero Leather",
-            image: "/images/projects/zephyr-aero-leather.PNG",
+            image: "/images/projects/ZAL.PNG",
             description: {
                 brief: "An e-commerce site built with Next.js, React and Tailwind, utilizing Stripe for payment and a headless WooCommerce",
                 long: "This was a site I helped build at PROJXON. It is an e-commerce site that sells leather products such as wallets, belts, bags and cases. The site is built with Next.js, React and Tailwind. It utilizes Stripe for payment and is synced to a headless WooCommerce. Users can log in or sign up or simply shop as a guest. Logged in users can also view their order history. I worked on various parts of the site including the order history and payment pages and the Stripe integration."
@@ -16,7 +16,7 @@ export default function ProjectsList() {
         },
         {
             name: "PROJXON website",
-            image: "/images/projects/projxon-website.png",
+            image: "/images/projects/projxon.PNG",
             description: {
                 brief: "PROJXON's website, built with Next.js and React with improved SEO and performance and deployed with AWS EC2.",
                 long: "I helped rebuild the PROJXON website from React to Next.js to improve SEO, working with three others over three weeks. I optimized code using DRY principles, integrated blogs and testimonials from a headless WordPress via the REST API, and enabled logged-in users to add testimonials. I implemented SSG and ISR for efficient data fetching and added metadata. This project enhanced my skills in React Bootstrap, internal API calls in Next.js, and balancing static and dynamic rendering for performance."
