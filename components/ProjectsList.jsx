d9edb4a107170d8552e38439c3714215208f6469
davenpos <davenpos@uwindsor.ca>
2025-07-07 18:28:03 -0400

    Added TypeScript as a skill to PROJXON website

--- components/ProjectsList.jsx
+++ components/ProjectsList.jsx
@@ -21,7 +21,7 @@ export default function ProjectsList() {
                 brief: "PROJXON's website, built with Next.js and React with improved SEO and performance and deployed with AWS EC2.",
                 long: "I helped rebuild the PROJXON website from React to Next.js to improve SEO, working with three others over three weeks. I optimized code using DRY principles, integrated blogs and testimonials from a headless WordPress via the REST API, and enabled logged-in users to add testimonials. I implemented SSG and ISR for efficient data fetching and added metadata. This project enhanced my skills in React Bootstrap, internal API calls in Next.js, and balancing static and dynamic rendering for performance."
             },
-            skills: ["HTML", "CSS", "JavaScript", "Next.js", "React", "WordPress", "Bootstrap", "AWS EC2"],
+            skills: ["HTML", "CSS", "JavaScript", "TypeScript", "Next.js", "React", "WordPress", "Bootstrap", "AWS EC2"],
             link: "https://www.projxon.com/",
             code: "https://github.com/PROJXON/ProjxonNext"
         },
